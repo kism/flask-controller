e0b1540bbece6f3b02a1017bd1f46d1d11af9c25
Kieran Gee <kieran.lost.the.game@gmail.com>
2024-01-05 17:33:49 +0800

    readme

--- _emulator/bizhawk/bizhawk_gba_grabwebinput.lua
+++ _emulator/bizhawk/bizhawk_gba_grabwebinput.lua
@@ -1,10 +1,35 @@
-INPUTBUFFER = {}
-LASTINPUT = {}
+--[[
+Copyright (c) 2023 Zunawe
+Copyright (c) 2023 KiSM
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+]]
+
+-- Bizhawk port of my mgba code, ty Zunawe for connector_bizhawk_generic.lua
 
 -- Input Mapping Table
 -- 10  ,9 ,8   ,7 ,6   ,5    ,4    ,3     ,2,1
 -- 512,256,128 ,64,32  ,16   ,8    ,4     ,2,1
 -- L  ,R  ,Down,Up,Left,Right,Start,Select,B,A
+
+INPUTBUFFER = {}
+LASTINPUT = {}
 
 local bizhawk_version = client.getversion()
 local bizhawk_major, bizhawk_minor, bizhawk_patch = bizhawk_version:match("(%d+)%.(%d+)%.?(%d*)")
