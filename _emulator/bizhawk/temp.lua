d1bb3cd5c4f917e880190bcd058934bbacdb5925
Kieran Gee <kieran.lost.the.game@gmail.com>
2024-01-06 08:44:54 +0800

    fix

--- _emulator/bizhawk/temp.lua
+++ _emulator/bizhawk/temp.lua
@@ -6,9 +6,9 @@ function send_receive()
 
         local p2, err = client_socket:receive(1)
         p2 = string.byte(p2)
-        p2 = p2 * 256
+        p2 = bit.lshift(p2, 8)
 
-        local p = p1 + p2
+        local p = bit.bor(p1, p2)
 
         table.insert(INPUTBUFFER, p)
     end
