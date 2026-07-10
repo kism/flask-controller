238dec8b922e1281eba31d147bfc75027846b8ef
Kieran Gee <kieran.lost.the.game@gmail.com>
2026-07-10 14:39:44 +0800

    fx

--- _emulator/generic_keyboard/generic_keyboard.py
+++ _emulator/generic_keyboard/generic_keyboard.py
@@ -17,7 +17,8 @@ if not DUMMY_SERVER:
 SERVER = None
 HOST = "localhost"
 PORT = 5001
-last_input_array = [False, False, False, False, False, False, False, False, False, False]
+KEYS = "qwertyuiop"
+last_state = 0
 
 
 # Create a socket object
@@ -32,48 +33,25 @@ server_socket.listen(5)
 logger.info("Server listening on %s:%s", HOST, PORT)
 
 
-def iterate_bits(num: int) -> list:
-    """Make a gross dict representing 10 bits."""
-    input_array = []
-    binary_representation = f"{num:b}".zfill(10)  # Assuming 32-bit integers for illustration
-
-    for _bit_position, bit_value in enumerate(binary_representation[::-1]):
-        logger.debug("Bit at position: %s:%s", _bit_position, bit_value)
-        new_bit_value = bit_value != "0"  # Why didn't I comment this, this is nonsense
-        input_array.append(new_bit_value)
-
-    return input_array
-
-
 def press_buttons(in_data: bytes) -> None:
-    """Bitwise compare to the socked and press/release depending."""
-    global last_input_array  # noqa: PLW0603 fine for this program/scale
+    """Press/release keys whose bits changed since the last packet."""
+    global last_state  # noqa: PLW0603 fine for this program/scale
 
-    my_int = int.from_bytes(in_data, "little")
+    state = int.from_bytes(in_data, "little")
+    logger.debug("New input: %010b", state)
 
-    input_array = iterate_bits(my_int)
+    for n, key in enumerate(KEYS):
+        bit = 1 << n
+        if (state ^ last_state) & bit:  # this bit changed
+            pressed = bool(state & bit)
+            if DUMMY_SERVER:
+                logger.info("%s %s", "Pressing" if pressed else "Releasing", key)
+            elif pressed:
+                pydirectinput.keyDown(key)
+            else:
+                pydirectinput.keyUp(key)
 
-    logger.debug(str(input_array))
-
-    logger.debug("New input: %s", int.from_bytes(in_data))
-    keys = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p"]
-    n = 0
-
-    for key in keys:
-        if input_array[n] != last_input_array[n]:
-            if input_array[n]:
-                if DUMMY_SERVER:
-                    logger.info("Pressing %s", key)
-                else:
-                    pydirectinput.keyDown(key)
-            else:  # noqa: PLR5501 HUH?
-                if DUMMY_SERVER:
-                    logger.info("Releasing %s", key)
-                else:
-                    pydirectinput.keyUp(key)
-        n = n + 1
-
-    last_input_array = input_array
+    last_state = state
 
 
 while True:
